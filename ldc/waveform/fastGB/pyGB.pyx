18256afb54732db1d3844e852f86ccac8da4d436
Maude Le Jeune <lejeune@apc.in2p3.fr>
2020-01-29 11:07:18 +0100

    add doc in fastGB

--- ldc/waveform/fastGB/pyGB.pyx
+++ ldc/waveform/fastGB/pyGB.pyx
@@ -16,10 +16,8 @@ import math
 # TODO:
 # orbits: use ldc.orbits in lisa.c
 # parameters: check pycbc conventions, give info on expected units, getter/setter tools
-# parameters for multiple sources and sum on TDI ?
 # time domain
 # output as frequency array
-
 
 
 YEAR = constants.Nature.SIDEREALYEAR_J2000DAY*24*60*60
@@ -33,7 +31,8 @@ cdef class pyGB:
 
     
     def __cinit__(self, orbits=None, T=6.2914560e7, delta_t=15):
-        """ Define C++ FastBinary dimensions and check that orbits are compatible. 
+        """ Define C++ FastBinary dimensions and check that orbits are
+        compatible.
         """
         if orbits is not None:
             if not isinstance(orbits, "AnalyticOrbits"):
@@ -45,10 +44,9 @@ cdef class pyGB:
         else:
             self.arm_length = 2.5e9
         self.T, self.delta_t = T, delta_t
-        
     
     def buffersize(self, f0, ampl, oversample):
-        """
+        """Get array dimension needed to compute TDI. 
         """
         Acut = simple_snr(f0,ampl,years=self.T/YEAR)
         mult = 8
@@ -65,18 +63,18 @@ cdef class pyGB:
         M = int(math.pow(2.0,1 + int(np.log(Acut)/np.log(2.0))))
         if(M > 8192):
             M = 8192
-
         M = N = max(M,N)
         N *= oversample
         return(N)
 
     def _parse_template(self, template):
-        """ TODO: 
+        """Return source parameters from dictionary.
+        
+        TODO: 
         - should be inherited from a general GB class
         - should check that keys exists
         - should also parse a vector ?
-        - should get unit and check them (but ParsUnit is not used so don't know how to do it)
-        
+        - should get unit and check them
         """
         f0 = template["Frequency"]
         fdot = template["FrequencyDerivative"]
@@ -91,7 +89,10 @@ cdef class pyGB:
     def get_fd_tdixyz(self, template=None, f0=None, fdot=None, ampl=None,
                       theta=None, phi=None, psi=None, incl=None, phi0=None,
                       oversample=1, simulator='synthlisa'):
-        """
+        """ Return TDI X,Y,Z in freq. domain. 
+        
+        f0 in Hz, fdot in Hz/s, ampl in strain, 
+        theta,phi,psi,incl,phi0 in rad. 
         """
         if template is not None:
             [f0, fdot, ampl, theta, phi, psi, incl, phi0] = self._parse_template(template)
@@ -124,6 +125,8 @@ cdef class pyGB:
         return freq,fX/df,fY/df,fZ/df
 
     def get_td_tdixyz(self):
+        """  Return TDI X,Y,Z in time domain. 
+        """
         fX, fY, fZ = self.get_fd_tdi()
         # todo compute inverse fft
         
