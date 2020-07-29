5eff917ce8b5efc2a4861fce9eb9aedb90a170e4
Maude Le Jeune <lejeune@apc.in2p3.fr>
2020-07-29 21:04:46 +0200

    use xarray index selection

--- ldc/common/tools/snr.py
+++ ldc/common/tools/snr.py
@@ -1,21 +1,5 @@
 import numpy as np
 
-def get_freq_indices(f1, f2, fmin=-1, fmax=-1, i=0):
-    """ Return indices of a common frequency range.  
-    """
-
-    ihmax = np.argwhere(f1 >=fmax)[0][0] if fmax>0 else len(f1)-1
-    ihmin = np.argwhere(f1 >=fmin)[0][0] if fmin>0 else 1
-    idmax = np.argwhere(f2 >=fmax)[0][0] if fmax>0 else len(f2)-1
-    idmin = np.argwhere(f2 >=fmin)[0][0] if fmin>0 else 1
-
-    if (f1[ihmin] != f2[idmin] or f1[ihmax] != f2[idmax]) and i==0:
-        fmin = max(f1[ihmin], f2[idmin])
-        fmax = min(f1[ihmax], f2[idmax])
-        return get_freq_indices(f1, f2, fmin=fmin, fmax=fmax, i=1)
-    else:
-        return ihmin,ihmax,idmin,idmax
-
 def compute_tdi_snr(source, noise, data=None, fmin=-1, fmax=-1, full_output=False):
     """ Compute SNR from TDI X,Y,Z. 
     
@@ -26,46 +10,56 @@ def compute_tdi_snr(source, noise, data=None, fmin=-1, fmax=-1, full_output=Fals
         data = source
 
     # freq indices selection
-    freq = np.array(source["X"].f)
     df = source["X"].attrs["df"]
-    ihmin,ihmax,idmin,idmax = get_freq_indices(freq, np.array(data["X"].f), fmin=fmin, fmax=fmax)
+    if fmin == -1:
+        fmin = source["X"].f[0] if source["X"].f[0]>0 else source["X"].f[1] 
+    if fmax == -1:
+        fmax = source["X"].f[-1]
     
     # inverse covariance matrix
+    freq = np.array(source["X"].sel(f=slice(fmin, fmax)).f)
     SXX = noise.psd(freq=freq, option='X')
     SXY = noise.psd(freq=freq, option='XY')
-    SXX, SXY = SXX[ihmin:ihmax], SXY[ihmin:ihmax]
-    Efact = 1/(SXX*SXX+SXX*SXY-2*SXY*SXY)
+    Efact = (SXX*SXX+SXX*SXY-2*SXY*SXY)
+    Efact[Efact==0] = np.inf
+    Efact = 1/Efact
     EXX = (SXX+SXY)*Efact
     EXY = -SXY*Efact
 
     dsnr = dict()
+    snr_tot = 0  # total SNR
+    cumsum = np.zeros((len(freq)))
+
     for k in ["X", "Y", "Z"]:     # individual SNR
-        snr = np.sum(np.real(data[k][idmin:idmax]*np.conj(source[k][ihmin:ihmax])/SXX))
+        d = data[k].sel(f=slice(fmin, fmax))
+        s = source[k].sel(f=slice(fmin, fmax))
+
+        snr = np.sum(np.real(d*np.conj(s)/SXX))
         snr *= 4.0*df
         dsnr[k+"2"] = snr
 
-    snr_tot = 0  # total SNR
-    cumsum = np.zeros((len(freq)))
-    for k in ["X", "Y", "Z"]:
-        snr = np.sum(np.real(data[k][idmin:idmax]*np.conj(source[k][ihmin:ihmax])*EXX))
+        snr = np.sum(np.real(d*np.conj(s)*EXX))
         snr_tot += snr
         if full_output:
-            cumsum[ihmin:ihmax]+= np.cumsum(np.real(data[k][idmin:idmax] *\
-                                                    np.conj(source[k][ihmin:ihmax])*EXX))
+            cumsum+= np.cumsum(np.real(d * np.conj(s)*EXX))
     for k1,k2 in [("X", "Y"), ("X", "Z"), ("Y", "Z")]:
-        snr = np.sum(np.real(data[k1][idmin:idmax]*np.conj(source[k2][ihmin:ihmax])*EXY))
-        snr+= np.sum(np.real(data[k2][idmin:idmax]*np.conj(source[k1][ihmin:ihmax])*EXY))
+        d1 = data[k1].sel(f=slice(fmin, fmax))
+        s1 = source[k1].sel(f=slice(fmin, fmax))
+        d2 = data[k2].sel(f=slice(fmin, fmax))
+        s2 = source[k2].sel(f=slice(fmin, fmax))
+
+        snr = np.sum(np.real(d1*np.conj(s2)*EXY))
+        snr+= np.sum(np.real(d2*np.conj(s1)*EXY))
         snr_tot += snr
         if full_output:
-            cumsum[ihmin:ihmax]+= np.cumsum(np.real(data[k1][idmin:idmax] *\
-                                                    np.conj(source[k2][ihmin:ihmax])*EXY))
-            cumsum[ihmin:ihmax]+= np.cumsum(np.real(data[k2][idmin:idmax] *\
-                                                    np.conj(source[k1][ihmin:ihmax])*EXY))
+            cumsum+= np.cumsum(np.real(d1 * np.conj(s2)*EXY))
+            cumsum+= np.cumsum(np.real(d2 * np.conj(s1)*EXY))
     snr_tot *= 4.*df
     dsnr["tot2"] = snr_tot
+
     if full_output:
         dsnr["cumsum"] = cumsum* 4.*df
-        dsnr["freq"] = freq[ihmin:ihmax]
+        dsnr["freq"] = freq
         dsnr["EXX"] = EXX
         dsnr["EXY"] = EXY
     return dsnr
