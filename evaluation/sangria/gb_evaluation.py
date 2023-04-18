d4bbf9880317f2475d0aa0b1195a15fc55429c80
Stefan Strub <stefan.strub3@gmail.com>
2023-04-18 10:44:05 +0200

    update eth solution read in

--- evaluation/sangria/gb_evaluation.py
+++ evaluation/sangria/gb_evaluation.py
@@ -88,7 +88,7 @@ class GBEval:
         """ Convert yml into a recarray
         """
         if self.team=="eth":
-            yml_solution = os.path.join(self.teamdir, "ETH-LDC2-sangria-training-v2-training-gb-cut.yaml")
+            yml_solution = os.path.join(self.teamdir, "ETH-LDC2-sangria-training-v2-training-gb-only-SNR5.yaml")
             if not os.path.exists(yml_solution):
                 download(yml_solution)
             csv_dir = os.path.join(self.teamdir, "Chain")
